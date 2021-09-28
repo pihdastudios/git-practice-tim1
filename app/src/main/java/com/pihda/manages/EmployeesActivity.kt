5655cf403fd85f51746a2e19cac2b507d0e2df33
Adib 444034 <62319714+pihdastudios@users.noreply.github.com>
2021-09-28 07:26:20 +0700

    add row

--- app/src/main/java/com/pihda/manages/EmployeesActivity.kt
+++ app/src/main/java/com/pihda/manages/EmployeesActivity.kt
@@ -1,8 +1,19 @@
 package com.pihda.manages
 
+import android.content.res.Resources
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.*
 
 class EmployeesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +21,26 @@ class EmployeesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_employees)
 //        val textView: TextView = findViewById(R.id.name_text)
 //        textView.text = Globals.restAddress
+
+    }
+
+    private fun addRow() {
+        var row: TableRow = TableRow(this).apply {
+            setBackgroundColor(0xF0F7F7)
+            setPadding(5.dp, 5.dp, 5.dp, 5.dp)
+            Modifier.weight(1)
+        }
+        var we =  Modifier.weight(2.0f, true)
+        val layout: TableLayout = findViewById(R.id.table_base)
+
+
     }
 //    override fun on() {
 //
 //    }
+    val Int.dp: Int
+        get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+    val Float.dp: Int
+        get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
 }
