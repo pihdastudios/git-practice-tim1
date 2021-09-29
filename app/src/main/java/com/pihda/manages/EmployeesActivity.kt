146a75c6915ca6f84cab84190a4a99ccd766d96f
Adib 444034 <62319714+pihdastudios@users.noreply.github.com>
2021-09-29 17:43:43 +0700

    add template layout

--- app/src/main/java/com/pihda/manages/EmployeesActivity.kt
+++ app/src/main/java/com/pihda/manages/EmployeesActivity.kt
@@ -6,12 +6,10 @@ import android.graphics.PorterDuffColorFilter
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.LinearLayout
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.*
 
@@ -19,23 +17,43 @@ class EmployeesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employees)
+
 //        val textView: TextView = findViewById(R.id.name_text)
 //        textView.text = Globals.restAddress
+        val button: Button = findViewById(R.id.refresh)
+        button.setOnClickListener() {
+            addRow(arrayOf("dsa", "asdfa", "dfsa", "asdf"))
+        }
 
     }
 
-    private fun addRow() {
+    private fun addRow(columns: Array<String>) {
         var row: TableRow = TableRow(this).apply {
             setBackgroundColor(0xF0F7F7)
             setPadding(5.dp, 5.dp, 5.dp, 5.dp)
-            Modifier.weight(1)
+            for (column in columns) {
+                addView(TextView(this@EmployeesActivity).apply {
+                    weightSum = 1f
+                    width = WRAP_CONTENT
+                    height = WRAP_CONTENT
+                    text = column
+                })
+            }
         }
-        var we =  Modifier.weight(2.0f, true)
         val layout: TableLayout = findViewById(R.id.table_base)
+        layout.post {
+            kotlin.run {
+                layout.addView(row)
+                layout.invalidate()
+                Log.d("asd", "dsf")
+
+            }
+        }
 
 
     }
-//    override fun on() {
+
+    //    override fun on() {
 //
 //    }
     val Int.dp: Int
